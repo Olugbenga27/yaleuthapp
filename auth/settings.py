5b3054dfd39c82d196e8f41caff779762ba36e0d
gbenga <agbengwiz@gmail.com>
2025-11-19 12:23:00 +0100

    serializers and email settings with media

--- auth/settings.py
+++ auth/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media configuration
+MEDIA_URL = '/image/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
+
+# email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = 'racwnncoomlqhuyv'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ userapi/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from . utils import send_email
+from .models import Profile
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSerializer()
+        fields = ['fullname','username','email','gender','phone','photo']  
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(write_only=True)
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    email = serializers.EmailField(write_only=True)
+    class Meta:
+        model = Profile
+        fields= ["fullname","username","password1","password2","email","gender","phone","photo"]
+
+    def validate(self, data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("password does not match")
+        return data
+    
+    def create(self, validated_data):
+        username= validated_data.pop('username')
+        email = validated_data.pop('email')
+        password = validated_data.pop('password1')
+
+        user = User.objects.create_user(username=username,password=password, email=email)
+        Profile = Profile.objects.create(
+            user = user,
+            fullname = validated_data['fullname'],
+            gender = validated_data['gender'],
+            phone = validated_data['phone'],
+            photo = validated_data['photo'],
+        )
+        # send email to the registered email
+        send_email(username,email)
+        return Profile
